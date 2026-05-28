1bfdceaa92043bb78572bfcc628c8ad13ab14715
kaue ogibowski <kaueogibowski@outlook.com>
2026-05-28 10:05:12 -0300

    ajuste de chances

--- acoesv2.py
+++ acoesv2.py
@@ -3,117 +3,141 @@ import random
 import textos  # Conectando com o seu arquivo de textos
 
 def aplicar_cenario(cenario, dicionario):
-    # Cenário 1: Bigtech crescem, fintech estáveis e cripto caem
+    # Cenário 1: Ações bigtech crescem, ações fintech permanecem estáveis e ações cripto caem
     if cenario == 1:
-        dicionario["bigtech_cons"] *= random.randint(0, 50) / 100
-        dicionario["bigtech_int"] *= random.randint(-5, 75) / 100
-        dicionario["bigtech_arr"] *= random.randint(-10, 150) / 100
+        # Bigtech: Cresce
+        dicionario["bigtech_cons"] *= random.randint(-10, 20) / 100
+        dicionario["bigtech_int"] *= random.randint(-20, 40) / 100
+        dicionario["bigtech_arr"] *= random.randint(-50, 100) / 100
 
-        dicionario["fintech_cons"] *= random.randint(0, 10) / 100
-        dicionario["fintech_int"] *= random.randint(-10, 50) / 100
-        dicionario["fintech_arr"] *= random.randint(-15, 75) / 100
+        # Fintech: Estável
+        dicionario["fintech_cons"] *= random.randint(-15, 15) / 100
+        dicionario["fintech_int"] *= random.randint(-30, 30) / 100
+        dicionario["fintech_arr"] *= random.randint(-75, 75) / 100
 
-        dicionario["cripto_cons"] *= random.randint(-10, 5) / 100
-        dicionario["cripto_int"] *= random.randint(-15, 10) / 100
-        dicionario["cripto_arr"] *= random.randint(-50, 15) / 100
+        # Cripto: Queda
+        dicionario["cripto_cons"] *= random.randint(-20, 10) / 100
+        dicionario["cripto_int"] *= random.randint(-40, 20) / 100
+        dicionario["cripto_arr"] *= random.randint(-100, 50) / 100
 
-    # Cenário 2: Bigtech estáveis, fintech caem e cripto crescem
+    # Cenário 2: Ações bigtech permanecem estáveis, ações fintech caem e ações cripto crescem
     elif cenario == 2:
-        dicionario["bigtech_cons"] *= random.randint(0, 10) / 100
-        dicionario["bigtech_int"] *= random.randint(-10, 50) / 100
-        dicionario["bigtech_arr"] *= random.randint(-15, 75) / 100
+        # Bigtech: Estável
+        dicionario["bigtech_cons"] *= random.randint(-15, 15) / 100
+        dicionario["bigtech_int"] *= random.randint(-30, 30) / 100
+        dicionario["bigtech_arr"] *= random.randint(-75, 75) / 100
 
-        dicionario["fintech_cons"] *= random.randint(-10, 5) / 100
-        dicionario["fintech_int"] *= random.randint(-15, 10) / 100
-        dicionario["fintech_arr"] *= random.randint(-50, 15) / 100
+        # Fintech: Queda
+        dicionario["fintech_cons"] *= random.randint(-20, 10) / 100
+        dicionario["fintech_int"] *= random.randint(-40, 20) / 100
+        dicionario["fintech_arr"] *= random.randint(-100, 50) / 100
 
-        dicionario["cripto_cons"] *= random.randint(0, 50) / 100
-        dicionario["cripto_int"] *= random.randint(-5, 75) / 100
-        dicionario["cripto_arr"] *= random.randint(-10, 150) / 100
+        # Cripto: Cresce
+        dicionario["cripto_cons"] *= random.randint(-10, 20) / 100
+        dicionario["cripto_int"] *= random.randint(-20, 40) / 100
+        dicionario["cripto_arr"] *= random.randint(-50, 100) / 100
 
-    # Cenário 3: Bigtech caem, fintech crescem e cripto estáveis
+    # Cenário 3: Ações bigtech caem, ações fintech crescem e ações cripto permanecem estáveis
     elif cenario == 3:
-        dicionario["bigtech_cons"] *= random.randint(-10, 5) / 100
-        dicionario["bigtech_int"] *= random.randint(-15, 10) / 100
-        dicionario["bigtech_arr"] *= random.randint(-50, 15) / 100
+        # Bigtech: Queda
+        dicionario["bigtech_cons"] *= random.randint(-20, 10) / 100
+        dicionario["bigtech_int"] *= random.randint(-40, 20) / 100
+        dicionario["bigtech_arr"] *= random.randint(-100, 50) / 100
 
-        dicionario["fintech_cons"] *= random.randint(0, 50) / 100
-        dicionario["fintech_int"] *= random.randint(-5, 75) / 100
-        dicionario["fintech_arr"] *= random.randint(-10, 150) / 100
+        # Fintech: Cresce
+        dicionario["fintech_cons"] *= random.randint(-10, 20) / 100
+        dicionario["fintech_int"] *= random.randint(-20, 40) / 100
+        dicionario["fintech_arr"] *= random.randint(-50, 100) / 100
 
-        dicionario["cripto_cons"] *= random.randint(0, 10) / 100
-        dicionario["cripto_int"] *= random.randint(-10, 50) / 100
-        dicionario["cripto_arr"] *= random.randint(-15, 75) / 100
+        # Cripto: Estável
+        dicionario["cripto_cons"] *= random.randint(-15, 15) / 100
+        dicionario["cripto_int"] *= random.randint(-30, 30) / 100
+        dicionario["cripto_arr"] *= random.randint(-75, 75) / 100
 
-    # Cenário 4: Bigtech crescem, fintech crescem e cripto caem
+    # Cenário 4: Ações bigtech crescem, ações fintech crescem e ações cripto caem
     elif cenario == 4:
-        dicionario["bigtech_cons"] *= random.randint(0, 50) / 100
-        dicionario["bigtech_int"] *= random.randint(-5, 75) / 100
-        dicionario["bigtech_arr"] *= random.randint(-10, 150) / 100
+        # Bigtech: Cresce
+        dicionario["bigtech_cons"] *= random.randint(-10, 20) / 100
+        dicionario["bigtech_int"] *= random.randint(-20, 40) / 100
+        dicionario["bigtech_arr"] *= random.randint(-50, 100) / 100
 
-        dicionario["fintech_cons"] *= random.randint(0, 50) / 100
-        dicionario["fintech_int"] *= random.randint(-5, 75) / 100
-        dicionario["fintech_arr"] *= random.randint(-10, 150) / 100
+        # Fintech: Cresce
+        dicionario["fintech_cons"] *= random.randint(-10, 20) / 100
+        dicionario["fintech_int"] *= random.randint(-20, 40) / 100
+        dicionario["fintech_arr"] *= random.randint(-50, 100) / 100
 
-        dicionario["cripto_cons"] *= random.randint(-10, 5) / 100
-        dicionario["cripto_int"] *= random.randint(-15, 10) / 100
-        dicionario["cripto_arr"] *= random.randint(-50, 15) / 100
+        # Cripto: Queda
+        dicionario["cripto_cons"] *= random.randint(-20, 10) / 100
+        dicionario["cripto_int"] *= random.randint(-40, 20) / 100
+        dicionario["cripto_arr"] *= random.randint(-100, 50) / 100
 
-    # Cenário 5: Bigtech crescem, fintech caem e cripto crescem
+    # Cenário 5: Ações bigtech crescem, ações fintech caem e ações cripto crescem
     elif cenario == 5:
-        dicionario["bigtech_cons"] *= random.randint(0, 50) / 100
-        dicionario["bigtech_int"] *= random.randint(-5, 75) / 100
-        dicionario["bigtech_arr"] *= random.randint(-10, 150) / 100
+        # Bigtech: Cresce
+        dicionario["bigtech_cons"] *= random.randint(-10, 20) / 100
+        dicionario["bigtech_int"] *= random.randint(-20, 40) / 100
+        dicionario["bigtech_arr"] *= random.randint(-50, 100) / 100
 
-        dicionario["fintech_cons"] *= random.randint(-10, 5) / 100
-        dicionario["fintech_int"] *= random.randint(-15, 10) / 100
-        dicionario["fintech_arr"] *= random.randint(-50, 15) / 100
+        # Fintech: Queda
+        dicionario["fintech_cons"] *= random.randint(-20, 10) / 100
+        dicionario["fintech_int"] *= random.randint(-40, 20) / 100
+        dicionario["fintech_arr"] *= random.randint(-100, 50) / 100
 
-        dicionario["cripto_cons"] *= random.randint(0, 50) / 100
-        dicionario["cripto_int"] *= random.randint(-5, 75) / 100
-        dicionario["cripto_arr"] *= random.randint(-10, 150) / 100
+        # Cripto: Cresce
+        dicionario["cripto_cons"] *= random.randint(-10, 20) / 100
+        dicionario["cripto_int"] *= random.randint(-20, 40) / 100
+        dicionario["cripto_arr"] *= random.randint(-50, 100) / 100
 
-    # Cenário 6: Bigtech caem, fintech crescem e cripto crescem
+    # Cenário 6: Ações bigtech caem, ações fintech crescem e ações cripto crescem
     elif cenario == 6:
-        dicionario["bigtech_cons"] *= random.randint(-10, 5) / 100
-        dicionario["bigtech_int"] *= random.randint(-15, 10) / 100
-        dicionario["bigtech_arr"] *= random.randint(-50, 15) / 100
+        # Bigtech: Queda
+        dicionario["bigtech_cons"] *= random.randint(-20, 10) / 100
+        dicionario["bigtech_int"] *= random.randint(-40, 20) / 100
+        dicionario["bigtech_arr"] *= random.randint(-100, 50) / 100
 
-        dicionario["fintech_cons"] *= random.randint(0, 50) / 100
-        dicionario["fintech_int"] *= random.randint(-5, 75) / 100
-        dicionario["fintech_arr"] *= random.randint(-10, 150) / 100
+        # Fintech: Cresce
+        dicionario["fintech_cons"] *= random.randint(-10, 20) / 100
+        dicionario["fintech_int"] *= random.randint(-20, 40) / 100
+        dicionario["fintech_arr"] *= random.randint(-50, 100) / 100
 
-        dicionario["cripto_cons"] *= random.randint(0, 50) / 100
-        dicionario["cripto_int"] *= random.randint(-5, 75) / 100
-        dicionario["cripto_arr"] *= random.randint(-10, 150) / 100
+        # Cripto: Cresce
+        dicionario["cripto_cons"] *= random.randint(-10, 20) / 100
+        dicionario["cripto_int"] *= random.randint(-20, 40) / 100
+        dicionario["cripto_arr"] *= random.randint(-50, 100) / 100
 
     # Cenário 7: Todas as ações crescem
     elif cenario == 7:
-        dicionario["bigtech_cons"] *= random.randint(0, 50) / 100
-        dicionario["bigtech_int"] *= random.randint(-5, 75) / 100
-        dicionario["bigtech_arr"] *= random.randint(-10, 150) / 100
+        # Bigtech: Cresce
+        dicionario["bigtech_cons"] *= random.randint(-10, 20) / 100
+        dicionario["bigtech_int"] *= random.randint(-20, 40) / 100
+        dicionario["bigtech_arr"] *= random.randint(-50, 100) / 100
 
-        dicionario["fintech_cons"] *= random.randint(0, 50) / 100
-        dicionario["fintech_int"] *= random.randint(-5, 75) / 100
-        dicionario["fintech_arr"] *= random.randint(-10, 150) / 100
+        # Fintech: Cresce
+        dicionario["fintech_cons"] *= random.randint(-10, 20) / 100
+        dicionario["fintech_int"] *= random.randint(-20, 40) / 100
+        dicionario["fintech_arr"] *= random.randint(-50, 100) / 100
 
-        dicionario["cripto_cons"] *= random.randint(0, 50) / 100
-        dicionario["cripto_int"] *= random.randint(-5, 75) / 100
-        dicionario["cripto_arr"] *= random.randint(-10, 150) / 100
+        # Cripto: Cresce
+        dicionario["cripto_cons"] *= random.randint(-10, 20) / 100
+        dicionario["cripto_int"] *= random.randint(-20, 40) / 100
+        dicionario["cripto_arr"] *= random.randint(-50, 100) / 100
 
     # Cenário 8: Todas as ações caem
     elif cenario == 8:
-        dicionario["bigtech_cons"] *= random.randint(-10, 5) / 100
-        dicionario["bigtech_int"] *= random.randint(-15, 10) / 100
-        dicionario["bigtech_arr"] *= random.randint(-50, 15) / 100
+        # Bigtech: Queda
+        dicionario["bigtech_cons"] *= random.randint(-20, 10) / 100
+        dicionario["bigtech_int"] *= random.randint(-40, 20) / 100
+        dicionario["bigtech_arr"] *= random.randint(-100, 50) / 100
 
-        dicionario["fintech_cons"] *= random.randint(-10, 5) / 100
-        dicionario["fintech_int"] *= random.randint(-15, 10) / 100
-        dicionario["fintech_arr"] *= random.randint(-50, 15) / 100
+        # Fintech: Queda
+        dicionario["fintech_cons"] *= random.randint(-20, 10) / 100
+        dicionario["fintech_int"] *= random.randint(-40, 20) / 100
+        dicionario["fintech_arr"] *= random.randint(-100, 50) / 100
 
-        dicionario["cripto_cons"] *= random.randint(-10, 5) / 100
-        dicionario["cripto_int"] *= random.randint(-15, 10) / 100
-        dicionario["cripto_arr"] *= random.randint(-50, 15) / 100
+        # Cripto: Queda
+        dicionario["cripto_cons"] *= random.randint(-20, 10) / 100
+        dicionario["cripto_int"] *= random.randint(-40, 20) / 100
+        dicionario["cripto_arr"] *= random.randint(-100, 50) / 100
 
     return dicionario
 
